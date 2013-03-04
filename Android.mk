fcf080a56d820aec0c395314fe9f0ca86c169760
Chetan Rao <chetan.rao@intel.com>
2013-03-04 12:02:23 +0200

    Move repo path of bootstub project to vendor/intel/boot/
    BZ: 100434
    
    Move the repo path of bootstub project from device/intel/
    to vendor/intel/boot/
    
    Change-Id: I446289211bec816496a8ec38e5d0870f7a232799
    Signed-off-by: Chetan Rao <chetan.rao@intel.com>
    Reviewed-on: http://android.intel.com:8080/101848
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- Android.mk
+++ Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 bootstub: $(PRODUCT_OUT)/boot/bootstub
 
 $(PRODUCT_OUT)/boot/bootstub:
-	cd device/intel/bootstub/ && CC=$(ANDROID_BUILD_TOP)/$($(my_prefix)CC) CMDLINE_SIZE=$(BOARD_KERNEL_CMDLINE_SIZE) make
+	cd vendor/intel/boot/bootstub/ && CC=$(ANDROID_BUILD_TOP)/$($(my_prefix)CC) CMDLINE_SIZE=$(BOARD_KERNEL_CMDLINE_SIZE) make
 	mkdir -p $(PRODUCT_OUT)/boot
-	mv device/intel/bootstub/bootstub $(PRODUCT_OUT)/boot
+	mv vendor/intel/boot/bootstub/bootstub $(PRODUCT_OUT)/boot
